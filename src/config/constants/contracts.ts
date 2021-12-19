7ae96dc5af3af1e345aecd54031452437de0dce4
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2021-12-19 17:20:56 -0600

    Adjust MasterChef

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,6 +1,6 @@
 export default {
   masterChef: {
-    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
+    97: '0xd4101EDC2B0c77a0A554D5446273e25218d386ae',
     56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
   },
   sousChef: {
